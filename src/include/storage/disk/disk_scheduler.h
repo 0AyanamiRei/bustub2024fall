6cbe36636b11819d65cfe7da980e8a3911e9a35a
0AyanamiRei <3244156674@qq.com>
2025-07-26 12:51:53 +0800

    2

--- src/include/storage/disk/disk_scheduler.h
+++ src/include/storage/disk/disk_scheduler.h
@@ -19,13 +19,11 @@
 #include "common/channel.h"
 #include "storage/disk/disk_manager.h"
 
-#define bk_threads 1024
+#define bk_threads 6400
 
 namespace bustub {
 
-/**
- * @brief Represents a Write or Read request for the DiskManager to execute.
- */
+
 struct DiskRequest {
   bool is_write_;
   char *data_;
@@ -33,47 +31,28 @@ struct DiskRequest {
   std::promise<bool> callback_;
 };
 
-/**
- * @brief The DiskScheduler schedules disk read and write operations.
- *
- * A request is scheduled by calling DiskScheduler::Schedule() with an appropriate DiskRequest object. The scheduler
- * maintains a background worker thread that processes the scheduled requests using the disk manager. The background
- * thread is created in the DiskScheduler constructor and joined in its destructor.
- */
+
 class DiskScheduler {
  public:
-  explicit DiskScheduler(DiskManager *disk_manager);
+  explicit DiskScheduler(DiskManager *disk_manager, int num_frames = 64);
   ~DiskScheduler();
 
-  void Schedule(DiskRequest r);
-  void StartWorkerThread();
+  void Schedule(DiskRequest r, frame_id_t frame_id);
 
   using DiskSchedulerPromise = std::promise<bool>;
   auto CreatePromise() -> DiskSchedulerPromise { return {}; };
 
-  /**
-   * @brief Increases the size of the database file to fit the specified number of pages.
-   *
-   * This function works like a dynamic array, where the capacity is doubled until all pages can fit.
-   *
-   * @param pages The number of pages the caller wants the file used for storage to support.
-   */
+  // This function works like a dynamic array, where the capacity is doubled until all pages can fit.
   void IncreaseDiskSpace(size_t pages) { disk_manager_->IncreaseDiskSpace(pages); }
 
-  /**
-   * @brief Deallocates a page on disk.
-   *
-   * Note: You should look at the documentation for `DeletePage` in `BufferPoolManager` before using this method.
-   * Also note: This is a no-op without a more complex data structure to track deallocated pages.
-   *
-   * @param page_id The page ID of the page to deallocate from disk.
-   */
+  // You should look at the documentation for `DeletePage` in `BufferPoolManager` before using this method.
+  // Note: This is a no-op without a more complex data structure to track deallocated pages.
   void DeallocatePage(page_id_t page_id) { disk_manager_->DeletePage(page_id); }
 
- private:
-  // std::mutex lock_;
+ private: 
   DiskManager *disk_manager_ __attribute__((__unused__));
-  Channel<std::optional<DiskRequest>> request_queue_[bk_threads];
-  std::optional<std::thread> background_thread_;
+  std::vector<Channel<std::optional<DiskRequest>>> request_queues_;
+  std::vector<std::thread> background_threads_;
+  bool _close{false};
 };
 }  // namespace bustub
